52cd4e03abcaa3ae707811dd241e4cce0152b98c
Sylvain périé <sylvain.perie@hotmail.fr>
2024-03-30 11:07:31 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/pixhubapi/PixhubApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pixhubapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PixhubApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PixhubApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/pixhubapi/PixhubApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pixhubapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PixhubApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
